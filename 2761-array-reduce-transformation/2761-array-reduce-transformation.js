e31333c0f629175d63982dd2687649c7c652cfb2
Eddie Lin <eddielin0926@gmail.com>
2025-10-03 02:40:01 +0800

    Time: 50 ms (31.6%), Space: 57.3 MB (5.34%) - LeetHub

---
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let accum = init;
+    for (num of nums) accum = fn(accum, num);
+    return accum;
+};
