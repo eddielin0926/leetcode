edf047be71ea90f8898f0989e56ce2f7a9409ff6
Eddie Lin <eddielin0926@gmail.com>
2025-10-14 11:25:19 +0800

    Time: 0 ms (100%), Space: 15.1 MB (65.72%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int left = 0;
-        int right = n - 1;
+        int left = 0, right = n - 1;
+
         while (left <= right) {
-            int middle = left + ((right - left) / 2);
-            cout << left << ' ' << right << endl;
-            if (nums[middle] == target) {
-                return middle;
-            } else if (nums[left] <= nums[middle]) { // left sorted portition
-                if (nums[left] <= target && target < nums[middle]) {
-                    right = middle - 1;
+            int pivot = left + ((right - left) / 2);
+            if (nums[pivot] == target) {
+                return pivot;
+            } else if (nums[left] <= nums[pivot]) {
+                if (nums[left] <= target && target <= nums[pivot]) {
+                    right = pivot - 1;
                 } else {
-                    left = middle + 1;
+                    left = pivot + 1;
                 }
-            } else { // right sorted portition
-                if (nums[middle] < target && target <= nums[right]) {
-                    left = middle + 1;
+            } else {
+                if (nums[pivot] <= target && target <= nums[right]) {
+                    left = pivot + 1;
                 } else {
-                    right = middle - 1;
+                    right = pivot - 1;
                 }
             }
         }
+
         return -1;
     }
 };
